a6c819213b16337150e5457e59690ba822b7960b
Thangaraj221 <thangrajm.23ece@kongu.edu>
2025-06-19 16:20:08 +0530

    day3

---
+++ React/pro/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Welcome from './Welcome'
+function App() {
+  
+
+  return (
+    <>
+     <Welcome name='gopal'country='anthiyur'/>
+    </>
+  )
+}
+
+export default App
